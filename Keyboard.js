67dc5b8059a5f736d2140dd6294c3eefff384d09
MohammadiHayatullah <mohammadihayatullah672@gmail.com>
2025-10-14 20:16:19 +0430

    And the final one

--- Keyboard.js
+++ Keyboard.js
@@ -1,17 +1,17 @@
 const audio = document.getElementById("keySound");
-document.addEventListener('keydown',(event)=>{
-    const key = event.key.toUpperCase();
-    const keyElement = document.getElementById(key);
-    if(keyElement){
-        keyElement.classList.add("active");
-        audio.currentTime = 0;
-        audio.play();
-    }
+document.addEventListener("keydown", (event) => {
+  const key = event.key.toUpperCase();
+  const keyElement = document.getElementById(key);
+  if (keyElement) {
+    keyElement.classList.add("active");
+    audio.currentTime = 0;
+    audio.play();
+  }
 });
-document.addEventListener('keyup',(event)=>{
-    const key = event.key.toUpperCase();
-    const keyElement = document.getElementById(key);
-    if(keyElement){
-        keyElement.classList.remove("active");
-    }
+document.addEventListener("keyup", (event) => {
+  const key = event.key.toUpperCase();
+  const keyElement = document.getElementById(key);
+  if (keyElement) {
+    keyElement.classList.remove("active");
+  }
 });
