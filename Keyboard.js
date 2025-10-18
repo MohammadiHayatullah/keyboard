e5de2c401c51d522d41f1f4f161d652abe5a745b
MohammadiHayatullah <mohammadihayatullah672@gmail.com>
2025-10-18 20:29:28 +0430

    After adding the complete keyboard

--- Keyboard.js
+++ Keyboard.js
@@ -11,7 +11,7 @@ const layout = [
   ["Tab","Q","W","E","R","T","Y","U","I","O","P","[","]","\\"],
   ["CapsLock","A","S","D","F","G","H","J","K","L",";","'","Enter"],
   ["Shift","Z","X","C","V","B","N","M",",",".","/","Shift"],
-  // ["Ctrl","Alt","Space","Alt","Ctrl"]
+  ["Ctrl","Alt","Space","Alt","Ctrl"]
 ];
 
 // ساخت کلیدها
