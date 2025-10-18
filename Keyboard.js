c2a76e61057ae5fed790824c77696349c1af06e6
MohammadiHayatullah <mohammadihayatullah672@gmail.com>
2025-10-18 16:37:29 +0430

    After some comments for better understanding the code

--- Keyboard.js
+++ Keyboard.js
@@ -1,4 +1,5 @@
 const audio = document.getElementById("keySound");
+//When you press a key on the keyboard, the corresponding key on the screen lights up and a sound plays.
 document.addEventListener("keydown", (event) => {
   const key = event.key.toUpperCase();
   const keyElement = document.getElementById(key);
@@ -7,6 +8,7 @@ document.addEventListener("keydown", (event) => {
     audio.currentTime = 0;
     audio.play();
   }
+//When you release the key, the corresponding key on the screen goes back to normal.
 });
 document.addEventListener("keyup", (event) => {
   const key = event.key.toUpperCase();
